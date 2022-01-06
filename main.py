fe2f6ff9da7cbdb4050ef02880fd6584d5999b08
Venkateshvenki404224 <75518037+Venkateshvenki404224@users.noreply.github.com>
2022-01-07 01:45:56 +0530

    Just for fun !

---
+++ main.py
@@ -0,0 +1,137 @@
+month = input("Pick the month you were born on:\n").lower()
+char = ""
+if month =="jan":
+    char =char + "I ate, "
+elif month =="feb":
+    char =char + "I Ran with, "
+elif month =="mar":
+    char =char + "I killed, "
+elif month=="april":
+    char=char + "I Licked "
+elif month =="may":
+    char =char + "I hugged, "
+elif month =="june":
+    char =char + "I kissed, "
+elif month=="july":
+    char=char + "I slept with, "
+elif month =="aug":
+    char =char + "I slapped, "
+elif month =="sep":
+    char =char + "I married, "
+elif month=="oct":
+    char=char + "I cooked, "
+elif month =="nov":
+    char =char + "I stabbed, "
+elif month=="dec":
+    char= char + "I kicked, "
+else:
+    print("Enter the correct value")
+print(char)
+day=input("Pick the day you were born on:\n")
+if day == "1":
+    char = char + "my lover, "
+elif day=="2":
+    char= char+"a day, "
+elif day=="3":
+    char= char+"the queen, "
+elif day=="4":
+    char= char+"a zombie, "
+elif day=="5":
+    char= char+"an alien, "
+elif day=="6":
+    char= char+"my mom, "
+elif day=="7":
+    char= char+"my teacher, "
+elif day=="8":
+    char= char+"an egg, "
+elif day=="9":
+    char= char+"a sheep, "
+elif day=="10":
+    char= char+"a television, "
+elif day=="11":
+    char= char+"my best friend, "
+elif day=="12":
+    char= char+"a ghost, "
+elif day=="13":
+    char= char+"my cat, "
+elif day=="14":
+    char= char+"santa claus, "
+elif day=="15":
+    char= char+"my school bag, "
+elif day=="16":
+    char = char+"my crush, "
+elif day =="17":
+    char = char+"a bear, "
+elif day =="18":
+    char = char+"toothbrush, "
+elif day =="19":
+    char = char+"unicorn, "
+elif day =="20":
+    char = char+"a cup of tea,"
+elif day =="21":
+    char = char+"my phone, "
+elif day =="22":
+    char = char+"my brother, "
+elif day =="23":
+    char = char+"a gun, "
+elif day =="24":
+    char = char+"my nose, "
+elif day =="25":
+    char = char+"a breakfast, "
+elif day =="26":
+    char = char+"the king, "
+elif day =="27":
+    char = char+"a milk, "
+elif day =="28":
+    char = char+"a book, "
+elif day =="29":
+    char = char+"a sword, "
+elif day =="30":
+    char= char+"love, "
+elif day =="31":
+    char = char+"a chicken, "
+else:
+    print("Enter the correct dob")
+print(char)
+name = input("Enter your First letter:\n").lower()
+if name == "a":
+    char = char + "because i'm sexy."
+elif name =="b":
+    char = char + "for good luck. "
+elif name =="c":
+    char = char + "for god's sake."
+elif name =="d":
+    char = char + "while watching TV. "
+elif name =="e":
+    char = char + "at my friend's house ."
+elif name =="f":
+    char = char + " beacuse i can. "
+elif name =="g":
+    char = char + "while eating lunch ."
+elif name =="h":
+    char = char + "bacause i hate that. "
+elif name =="i":
+    char = char + "and i'm proud of it. "
+elif name =="j":
+    char = char + "because it's my hobby . "
+elif name =="k":
+    char = char + "covered with chocolates."
+elif name =="l":
+    char = char + "because i'am horny."
+elif name =="m":
+    char = char + "inside a car. "
+elif name =="n":
+    char = char + "and i liked it. "
+elif name =="o":
+    char = char + "covered with honey."
+elif name =="p":
+    char = char + "when my friend died."
+elif name =="q":
+    char = char + "and it was amazing ."
+elif name =="r":
+    char = char + "to kill time."
+elif name =="s":
+    char = char + "because my brain is small ."
+else:
+    char = char + "because i'm awesome"
+print(char)
